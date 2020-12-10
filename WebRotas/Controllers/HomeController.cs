24caab2b40bce4a7f22e431d5c03d93cac6322e2
vitormmh <74314572+vitormmh@users.noreply.github.com>
2020-12-10 18:45:30 +0000

    Add project files.

---
+++ WebRotas/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebRotas.Models;
+
+namespace WebRotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEnumerable<Noticia> todasAsNoticias;
+
+        public HomeController()
+        {
+            todasAsNoticias = new Noticia().TodasAsNoticias().OrderByDescending(m=>m.Data);
+
+        }
+
+        public ActionResult Index()
+        {
+            var ultimasNoticias = todasAsNoticias.Take(3);
+            var todasAsCategorias = todasAsNoticias.Select(m =>m.Categoria).Distinct().ToList();
+            ViewBag.Categorias = todasAsCategorias; //viewbag para passar as categorias para a view
+            // passar tambem as ultimas noticias para a view
+
+            return View(ultimasNoticias);
+        }
+
+        public ActionResult TodasAsNoticias()
+        {
+            return View(todasAsNoticias);
+        }
+
+        //porquê os parametros categoria e titulo se só vamos passa noticiaId ?
+        //porque vamos necessitar de passar categoria e titulo para fazer a nossa URL amigavel - (ver rotas) 
+        public ActionResult MostraNoticia(int noticiaId, string categoria, string titulo)
+        {
+            
+            return View(todasAsNoticias.FirstOrDefault(n=>n.NoticiaId==noticiaId));
+        }
+
+        public ActionResult MostraCategoria(string categoria)
+        {
+            var categoriaEspecifica = todasAsNoticias.Where(c => c.Categoria == categoria).ToList();
+            ViewBag.Categoria = categoria;
+            return View(categoriaEspecifica);
+        }
+    }
+}
---
+++ WebRotas/Models/Noticia.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebRotas.Models
+{
+    public class Noticia
+    {
+        public int NoticiaId { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Categoria { get; set; }
+        [DisplayFormat(DataFormatString = "{0://dd/MM/yyyy}")]
+        public DateTime Data { get; set; }
+
+
+        public IEnumerable<Noticia> TodasAsNoticias()
+        {
+            var retorno = new Collection<Noticia>
+        {
+                    new Noticia {
+                    NoticiaId = 1,
+                    Titulo = "Ronaldo Campeão",
+                    Categoria = "Desporto",
+                    Conteudo = "Cristiano Ronaldo consagrou-se novamente campeão mundial e o melhor goleador",
+                    Data = new DateTime(2019, 12, 3)
+                    },
+                 new Noticia
+                 {
+                     NoticiaId = 2,
+                     Titulo = "Presidente prepara nova candidatura",
+                     Categoria = "Politica",
+                     Conteudo = "O Presidente da Republica prepara nova candidatura para o segundo mandato",
+                     Data = new DateTime(2019, 11, 5)
+                 },
+                  new Noticia
+                  {
+                      NoticiaId = 3,
+                      Titulo = "Presidente prepara nova candidatura",
+                      Categoria = "Politica",
+                      Conteudo = "O Presidente da Republica prepara nova candidatura para o segundo mandato",
+                      Data = new DateTime(2019, 11, 5)
+                  },
+
+                      new Noticia
+                      {
+                          NoticiaId = 4,
+                          Titulo = "Tanto para saber e em tão pouco tempo",
+                          Categoria = "Atualidade",
+                          Conteudo = "O Tempo é inimigo da perfeição... Se eu quiser aprender tenho que trabalhar e dedicarme totalmente, depois como sei que sou capaz, os resultados hão-de vir  ",
+                          Data = new DateTime(2019, 12, 10)
+                      }
+
+
+        };
+            return retorno;
+        }
+
+    }
+}
